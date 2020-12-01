0d33bdb7c994b3c8aa4bb43ce93fedfb21942d1a
Rocio01 <zrmartinezg@gmail.com>
2020-12-01 14:01:01 -0500

    Add model and user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  # get 'home/index'
+  root "home#index"
+  # get "users" => "users#index"
+  # get "users/new" =>  "users#new"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
