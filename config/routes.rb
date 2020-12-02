8698de90ed3379eda81c688eed9716c887c7c56f
Rocio01 <zrmartinezg@gmail.com>
2020-12-02 10:37:34 -0500

    Add sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   #  get "users/:id" => "users#show"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:new, :create, :destroy]
 end
+
+
