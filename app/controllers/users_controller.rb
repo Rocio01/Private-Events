a675f323dff81b2750c1b17c174d3b50858ac2b3
Rocio01 <zrmartinezg@gmail.com>
2020-12-02 13:16:23 -0500

    Add partial _alerts.html.erb, add table in user/index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,6 +26,11 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to "/users"
+    end
   
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # get "users" => "users#index"
   # get "users/new" => "users#new"
   # post "users" => "users#create"
-  #  get "users/:id" => "users#show"
+  #  get "users/:id" => "users#destroy "
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:new, :create, :destroy]
