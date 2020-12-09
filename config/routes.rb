aacea496b6826da4e3367a95aa3d2f9ecdcc61a3
karthick <karthylock@gmail.com>
2020-12-10 00:17:09 +0530

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,11 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  resources :users
-  resources :events
-  resources :attendances
-
+  root 'sessions#welcome'
+  resources :users, only: [:new, :create, :show]
+  resources :events do
+    post 'attend', on: :member
+  end
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
-  post '/logout' => 'sessions#destroy'
-
-  get '/signup' => 'users#new'
-  post '/users' => 'users#create'
-
-  get '/my-events' => 'users#show'
-
-  get '/attendees' => 'events#show'
-
-  root 'events#index'
+  delete '/logout' => 'sessions#destroy'
+  get '/welcome' => 'sessions#welcome'
 end
