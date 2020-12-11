3ee1aea28674771e433df7aa698f7e5865f6dee4
karthick <karthylock@gmail.com>
2020-12-12 01:35:22 +0530

    remove linter errors

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -2,12 +2,10 @@ module EventsHelper
   def event_params
     params.require(:event).permit(:name, :description, :date)
   end
-  
+
   def logged_in
-    @upcoming_events.each do |event| 
-     if logged_in?
-       render 'attendees/attendence_form', event: event
-     end 
+    @upcoming_events.each do |event|
+      render 'attendees/attendence_form', event: event if logged_in?
     end
   end
 end
